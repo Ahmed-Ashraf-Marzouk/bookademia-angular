7a383f75afc2f2e477ccb129b6906a456daf45eb
AhmedMohammedKhorkhash <khorkhash3@gmail.com>
2022-05-21 21:45:54 +0200

    routing book-details-homepage images to bookResult

--- src/app/book-details-homepage/book-details-homepage.component.ts
+++ src/app/book-details-homepage/book-details-homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router"
 
 @Component({
   selector: 'app-book-details-homepage',
@@ -9,7 +10,14 @@ export class BookDetailsHomepageComponent implements OnInit {
   name = "harry"
   price = "150.00"
   imgsrc = "assets/images/action-thriller-book-cover-design-template-3675ae3e3ac7ee095fc793ab61b812cc_screen.jpg"
-  constructor() { }
+  constructor(private router: Router) { }
+
+  goToBookResult() {
+
+    this.router.navigate(['/book-result'])
+
+  }
+
 
   ngOnInit(): void {
   }
