e971bdab58021dc86e3c4f8911726d84c45181d8
abdelrahman mahmoud <abdelrahmanmahmoudam2000@gmail.com>
2022-05-21 16:52:41 +0200

    update collection-books to take variable and show using interpolation

--- src/app/bookcollection/bookcollection.component.ts
+++ src/app/bookcollection/bookcollection.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-bookcollection',
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class BookcollectionComponent implements OnInit {
 
   constructor() { }
-
+  @Input() title = "Collection of best"
   ngOnInit(): void {
   }
 
